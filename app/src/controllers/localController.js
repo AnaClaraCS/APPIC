56a26db77eb0d8410a12988fbbeec8b3cef0e690
AnaClaraCS <anaclaracostasouza@yahoo.com.br>
2024-06-13 13:59:59 -0300

    Excluir local exclui leituras desse local

--- app/src/controllers/localController.js
+++ app/src/controllers/localController.js
@@ -2,10 +2,12 @@ import { getDatabase, ref, set, get, update, remove, push } from 'firebase/datab
 //import { getDatabase, ref, set, get, update, remove, push } from '../firebase.js';
 import { database } from '../firebase.js';
 import Local from '../models/local.js';
+import LeituraController from './leituraController.js';
 
 class LocalController {
   constructor() {
     this.database = database;
+    this.leituraController = new LeituraController();
   }
 
   // Criar um novo local
@@ -40,8 +42,19 @@ class LocalController {
   }
 
   // Deletar um local
-  async deletarLocal(id) {
-    await remove(ref(this.database, `locais/${id}`));
+  async deletarLocal(idLocal) {
+
+    const leituras = await this.leituraController.obterLeituras();
+
+    // Deletar leituras associadas ao local
+    const deletePromises = leituras
+      .filter(leitura => leitura.idLocal === idLocal)
+      .map(leitura => this.leituraController.deletarLeitura(leitura.idLeitura));
+      deletarLeiturasPorLocal
+    // Esperar que todas as leituras sejam deletadas
+    await Promise.all(deletePromises);
+
+    await remove(ref(this.database, `locais/${idLocal}`));
   }
 }
 
